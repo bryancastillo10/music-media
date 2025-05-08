6a91f0f95bcd63f52e38f77fa56ffc15a52f5720
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-05-08 15:17:50 +0800

    🔗 route: connect song routes to controller

--- server/song/song.route.ts
+++ server/song/song.route.ts
@@ -1,13 +1,16 @@
 import express from "express";
 
 import { protectRoute } from "@/auth/auth.config";
+import { verifyAdmin } from "@/infrastructure/middleware/verifyAdmin.middleware";
+
+import { songController } from "@/song/song.config";
 
 const router = express.Router();
 
-router.get("/", protectRoute, ()=> {});
+router.get("/", protectRoute, verifyAdmin, songController.fetchAllSongs);
 
-router.get("/featured", ()=> {});
-router.get("/my-playlist", ()=> {});
-router.get("/trending", ()=> {});
+router.get("/featured", protectRoute, songController.fetchFeatured);
+router.get("/my-playlist", protectRoute, songController.fetchMyPlaylist);
+router.get("/trending", protectRoute, songController.fetchTrending);
 
 export default router;
