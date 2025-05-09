786fdcf0843a1750415ee710d2384f74401aa954
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-05-09 13:59:55 +0800

    🧩 component: log in page

--- client/src/features/auth/components/Login.tsx
+++ client/src/features/auth/components/Login.tsx
@@ -1,11 +1,12 @@
+import { LoginForm } from "@/features/auth/lib/login-form"
 
-
-const Login = () => {
+export default function LoginPage() {
   return (
-	<div>
-	  <h1>Login Page</h1>
-	</div>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
+    </div>
   )
 }
 
-export default Login;
