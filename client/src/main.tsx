36e712105dd2ab6979ea199bd43cd37883338bdd
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-05-09 11:03:35 +0800

    🔗 client routing: set up tanstack dependency

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+
+import { routeTree } from '@/routeTree.gen';
+import "./index.css";
+
+import PageNotFound from '@/components/navigations/PageNotFound';
+import Providers from '@/components/providers/Providers';
+
+const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: () => <PageNotFound/>
+});
+
+declare module '@tanstack/react-router' {
+  interface Register{
+    router: typeof router;
+  }
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Providers>
+      <RouterProvider router={router} />
+    </Providers>  
   </StrictMode>,
 )
