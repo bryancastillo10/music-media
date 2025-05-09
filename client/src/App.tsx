8c7088e4b7d2b1c2ff2a320a7387658061816686
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-05-09 10:07:10 +0800

    🖼️ style:  configure theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@
 
 const App = () => {
   return (
-    <div className="bg-teal-500 p-4">
+    <div className="bg-primary dark:bg-primary p-4">
       <h1 className="font-semibold">Test Render Message</h1>
     </div>
   )
